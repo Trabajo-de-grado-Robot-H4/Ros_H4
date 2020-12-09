ccae2dd6ce5fd01e2f46b81f90e62f38304412dd
CristianGoFI <74423403+CristianGoFI@users.noreply.github.com>
2020-12-09 12:33:48 -0500

    Update RLP_Encoder.py

--- RLP_Encoder.py
+++ RLP_Encoder.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import String
+from geometry_msgs.msg import Pose
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
@@ -14,7 +14,7 @@ def listener():
     # run simultaneously.
     rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber("chatter", String, callback)
+    rospy.Subscriber("Encoder", Pose, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
