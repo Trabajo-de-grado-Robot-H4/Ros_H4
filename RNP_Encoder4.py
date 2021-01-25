04799a68fc397369c2d58a8d1901afffe7df8f71
CristianGoFI <74423403+CristianGoFI@users.noreply.github.com>
2021-01-25 18:21:11 -0500

    Update RNP_Encoder4.py

--- RNP_Encoder4.py
+++ RNP_Encoder4.py
@@ -56,7 +56,7 @@ def destroy():
 """ funcion que publica los datos del encoder """
 def talker():
     pub = rospy.Publisher('Encoder', Pose, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
+    rospy.init_node('talker4', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         
