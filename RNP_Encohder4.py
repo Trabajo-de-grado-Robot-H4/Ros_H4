a1cdf9653a5efe110e58bd4e42d060f7358c73cb
CristianGoFI <74423403+CristianGoFI@users.noreply.github.com>
2021-01-25 11:35:08 -0500

    Update RNP_Encohder4.py

--- RNP_Encohder4.py
+++ RNP_Encohder4.py
@@ -44,11 +44,9 @@ def setup():
     GPIO.setup(RoBPin4, GPIO.IN)
 """ funcion que lee el encoder """
 def rotaryDeal():
- global flag1
- global flag2,flag3,flag4
+ global flag1,flag2,flag3,flag4
  global Last_RoB_Status1,Last_RoB_Status2,Last_RoB_Status3,Last_RoB_Status4 
- global Current_RoB_Status1
- global Current_RoB_Status2,Current_RoB_Status3,Current_RoB_Status4
+ global Current_RoB_Status1,Current_RoB_Status2,Current_RoB_Status3,Current_RoB_Status4
  global globalCounter1,globalCounter2,globalCounter3,globalCounter4
  global grados1, grados2, grados3, grados4
  global gain
