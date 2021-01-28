e8cab2d7186fb3d52c0b87f2c5ee1d97d3b9560e
Dfsuarez97 <74423258+Dfsuarez97@users.noreply.github.com>
2021-01-28 16:08:02 -0500

    Update Discard.py

--- Discard.py
+++ Discard.py
@@ -12,12 +12,13 @@ MotorE1 = 18
 """ Declaracion de variables """
 Esfuerzo =0
 Last_esfuerzo=0
+GPIO.setmode(GPIO.BCM)
 p = GPIO.PWM(MotorE1, 100)# Creamos la instancia PWM con el GPIO a utilizar y la frecuencia de la señal PWM
 p.start(0)  #Inicializamos el objeto PWM
 
 def setup():
 
-    GPIO.setmode(GPIO.BCM)
+    
     GPIO.setwarnings(False)
 
     """ Setup del motor """
