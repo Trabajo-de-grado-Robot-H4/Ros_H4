7809aa761df8867c6c13661cb1d1c33e781bf151
CristianGoFI <74423403+CristianGoFI@users.noreply.github.com>
2021-01-27 23:56:40 -0500

    Update RNL_Motor.py

--- RNL_Motor.py
+++ RNL_Motor.py
@@ -27,14 +27,14 @@ def callback(data):
     p = GPIO.PWM(MotorE1, 100)  # Creamos la instancia PWM con el GPIO a utilizar y la frecuencia de la señal PWM
    
 
-  while True:
-    p.start(0)  #Inicializamos el objeto PWM
-    if Esfuerzo > 0:
+    while True:
+      p.start(0)  #Inicializamos el objeto PWM
+      if Esfuerzo > 0:
          GPIO.output(MotorIN1,GPIO.HIGH)  # Establecemos el sentido de giro con los pines IN1 e IN2
          GPIO.output(MotorIN2,GPIO.LOW)   # Establecemos el sentido de giro con los pines IN1 e IN2
          p.ChangeDutyCycle(Esfuerzo)
          rospy.loginfo(rospy.get_caller_id() + 'I heard %f', Esfuerzo)
-    else:
+      else:
          GPIO.output(MotorIN1,GPIO.LOW)   # Establecemos el sentido de giro con los pines IN1 e IN2
          GPIO.output(MotorIN2,GPIO.HIGH)  # Establecemos el sentido de giro con los pines IN1 e IN2
          p.ChangeDutyCycle(abs(Esfuerzo))
