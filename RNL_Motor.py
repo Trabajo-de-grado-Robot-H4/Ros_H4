67acdc4bc5582cd349a599266d8c71baaeb85877
crstian <cristiancg@unicauca.edu.co>
2021-01-28 13:58:32 -0500

    usando multi hilo con funcion listener sin multihilo.

--- RNL_Motor.py
+++ RNL_Motor.py
@@ -3,16 +3,22 @@ import rospy
 from geometry_msgs.msg import Pose
 import RPi.GPIO as GPIO
 import time
-
+import threading
 
 """Pines usados """
 MotorIN1 = 15
 MotorIN2 = 14
 MotorE1 = 18
+""" Declaracion de variables """
+Esfuerzo =0
+Last_esfuerzo=0
+
 
 def setup():
 
     GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
     """ Setup del motor """
     GPIO.setup(MotorIN1,GPIO.OUT)
     GPIO.setup(MotorIN2,GPIO.OUT)
@@ -22,22 +28,9 @@ def setup():
 """inicio del programa """
 def callback(data):
     Esfuerzo = data.position.x
-    p = GPIO.PWM(MotorE1, 50)  # Creamos la instancia PWM con el GPIO a utilizar y la frecuencia de la señal PWM
-    p.start(0)  #Inicializamos el objeto PWM
-
-
-     if Esfuerzo > 0:
-         GPIO.output(MotorIN1,GPIO.HIGH)  # Establecemos el sentido de giro con los pines IN1 e IN2
-         GPIO.output(MotorIN2,GPIO.LOW)   # Establecemos el sentido de giro con los pines IN1 e IN2
-         p.ChangeDutyCycle(Esfuerzo)
-         rospy.loginfo(rospy.get_caller_id() + 'I heard %f', Esfuerzo)
-     else:
-         GPIO.output(MotorIN1,GPIO.LOW)   # Establecemos el sentido de giro con los pines IN1 e IN2
-         GPIO.output(MotorIN2,GPIO.HIGH)  # Establecemos el sentido de giro con los pines IN1 e IN2
-         p.ChangeDutyCycle(abs(Esfuerzo))
-         rospy.loginfo(rospy.get_caller_id() + 'I heard %f', Esfuerzo)
-
-
+    rospy.loginfo(rospy.get_caller_id() + 'I heard %f', Esfuerzo)
+def destroy():
+        GPIO.cleanup()
 def listener():
 
     # In ROS, nodes are uniquely named. If two nodes with the same
@@ -51,11 +44,34 @@ def listener():
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
-    #ese te un cambio
+
+
+def pwm():
+
+    p = GPIO.PWM(MotorE1, 100)  # Creamos la instancia PWM con el GPIO a utilizar y la frecuencia de la señal PWM
+    p.start(0)  #Inicializamos el objeto PWM
+
+    while True:
+        global Esfuerzo
+        if Esfuerzo > 0:
+            GPIO.output(MotorIN1,GPIO.HIGH)  # Establecemos el sentido de giro con los pines IN1 e IN2
+            GPIO.output(MotorIN2,GPIO.LOW)   # Establecemos el sentido de giro con los pines IN1 e IN2
+            p.ChangeDutyCycle(Esfuerzo)
+            logging.debug(Esfuerzo)
+
+        else:
+            GPIO.output(MotorIN1,GPIO.LOW)   # Establecemos el sentido de giro con los pines IN1 e IN2
+            GPIO.output(MotorIN2,GPIO.HIGH)  # Establecemos el sentido de giro con los pines IN1 e IN2
+            p.ChangeDutyCycle(abs(Esfuerzo))
+            logging.debug(Esfuerzo)
+
 
 if __name__ == '__main__':
     setup()
     try:
             listener()
+            worker = threading.Thread(name='pwm1',target=pwm,demon=True)
+            worker.start()
     except rospy.ROSInterruptException:
+            destroy()
             pass
