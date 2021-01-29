3dd234e4f71cd42b23f2f8cd81335284ed8fa40c
crstian <cristiancg@unicauca.edu.co>
2021-01-29 01:19:14 -0500

    sacar del callback

--- RNL_Motor.py
+++ RNL_Motor.py
@@ -5,68 +5,24 @@ import RPi.GPIO as GPIO
 import time
 import threading
 
-"""Pines usados """
-MotorIN1 = 15
-MotorIN2 = 14
-MotorE1 = 18
-""" Declaracion de variables """
-Esfuerzo =0
-Last_esfuerzo=0
+class Listener(object):
+   def __init__(self):
+     self.flag = True
+     self.sub = rospy.Subscriber('Datosmotor', Pose, self.echo)
 
-
-def setup():
-
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-
-    """ Setup del motor """
-    GPIO.setup(MotorIN1,GPIO.OUT)
-    GPIO.setup(MotorIN2,GPIO.OUT)
-    GPIO.setup(MotorE1,GPIO.OUT)
-
-
-"""inicio del programa """
-def callback(data):
-    global Esfuerzo
-    Esfuerzo = data.position.x
-    rospy.loginfo(rospy.get_caller_id() + 'I heard %f', Esfuerzo)
-        
-def pwm():
-    global Esfuerzo
-    rate = rospy.Rate(Esfuerzo) # ROS Rate at 5Hz
-    GPIO.output(MotorIN1,GPIO.HIGH)  # Establecemos el sentido de giro con los pines IN1 e IN2
-    GPIO.output(MotorIN2,GPIO.LOW)
-    
-    while not rospy.is_shutdown():
-        GPIO.output(MotorE1,GPIO.HIGH)
-        rate.sleep()
-        
-def destroy():
-        GPIO.cleanup()
-        
-def listener():
-
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('ListenerM', anonymous=True)
-
-    rospy.Subscriber("Datosmotor", Pose, callback)
-    global Esfuerzo 
-
-    # spin() simply keeps python from exiting until this node is stopped
-    worker = threading.Thread(target=pwm)
-    worker.start()
-    rospy.spin()
+   def echo(self, data):  # data.msg can be 'stop' string or any other string
+     if data.position.x == 0:
+       self.flag = False
+       self.return_value = data.foo
+     else:
+       rospy.loginfo(data.position.x)
 
 
 if __name__ == '__main__':
-    setup()
-    try:
-            listener()
-            
-    except rospy.ROSInterruptException:
-            destroy()
-            pass
+  rospy.init_node('listener')
+  list = Listener()
+
+  while list.flag:
+    rospy.sleep(1)
+
+  print 'Value was non-stop'
