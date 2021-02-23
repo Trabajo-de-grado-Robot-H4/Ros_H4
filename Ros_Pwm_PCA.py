e0541fc7cfa07cddd0d1f6af864eed7cd7eafba7
CristianGoFI <74423403+CristianGoFI@users.noreply.github.com>
2021-02-23 16:26:48 -0500

    Aqui deje preparado la funcion de lectura, aun no se usa

--- Ros_Pwm_PCA.py
+++ Ros_Pwm_PCA.py
@@ -3,13 +3,31 @@ import board
 import busio
 import adafruit_pca968
 
-#inicializacion de protocoño de comunicacion I2C
-i2c = busio.I2C(board.SCL, board.SDA)
-pca = adafruit_pca9685.PCA9685(i2c)
-
-#asignamos la frecuencia del PWM
-pca.frequency = 60
 
 
-#enviamos el PWM al Pin deseado, con 
-pca.channels[0].duty_cycle = 0x7FFF
+
+def setup():
+    #inicializacion de protocoño de comunicacion I2C
+    i2c = busio.I2C(board.SCL, board.SDA)
+    pca = adafruit_pca9685.PCA9685(i2c)
+    #asignamos la frecuencia del PWM
+    pca.frequency = 60
+    #iniciamos el canal a usar
+    Canal0 = pca.channels[0]
+
+#enviamos el PWM al Pin deseado, con
+def loop():
+    Canal0.duty_cycle = 0x7FFF
+
+def Input_data():
+    PWM=float(input("Inserte PWM"))
+
+    print(PWM)
+    return(PWM)
+
+
+if __name__ == '__main__':     # Program start from here
+        setup()
+        try:
+               loop()
+        except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program d>
