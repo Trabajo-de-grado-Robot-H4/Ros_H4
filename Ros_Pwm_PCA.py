984e6495bb87258d28a541f21265a1120f9b71f9
CristianGoFI <74423403+CristianGoFI@users.noreply.github.com>
2021-02-23 15:59:46 -0500

    Create Ros_Pwm_PCA.py

---
+++ Ros_Pwm_PCA.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import board
+import busio
+import adafruit_pca968
+
+#inicializacion de protocoño de comunicacion I2C
+i2c = busio.I2C(board.SCL, board.SDA)
+pca = adafruit_pca9685.PCA9685(i2c)
+
+#asignamos la frecuencia del PWM
+pca.frequency = 60
+
+
+#enviamos el PWM al Pin deseado, con 
+pca.channels[0].duty_cycle = 0x7FFF
