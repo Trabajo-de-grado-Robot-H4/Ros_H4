2795d871a5562a7891b2f5b04a79f5d31a07507a
CristianGoFI <74423403+CristianGoFI@users.noreply.github.com>
2021-03-11 15:06:28 -0500

    dejando fijo valores de pwm

--- Ros_Pwm_PCA.py
+++ Ros_Pwm_PCA.py
@@ -13,12 +13,10 @@ pca.frequency = 60
 
 #enviamos el PWM al Pin deseado, con
 
-Datos_pid=[]
-for v in range(4):
-    val=int(input("Inserte pwm en los canales 1,2,3,4 en este orden: "))
-    Datos_pid.append(val)
 
-pca.channels[0].duty_cycle = Datos_pid[0]
-pca.channels[1].duty_cycle = Datos_pid[1]
-pca.channels[2].duty_cycle = Datos_pid[2]
-pca.channels[3].duty_cycle = Datos_pid[3]
+val=int(input("Inserte pwm en el canal 0: "))
+
+pca.channels[0].duty_cycle = val
+pca.channels[1].duty_cycle = 0
+pca.channels[2].duty_cycle = 60000
+pca.channels[3].duty_cycle = 0
