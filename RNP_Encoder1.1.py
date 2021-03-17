fb5366f0f9baa5f4e988f710866a068a25f5612e
CristianGoFI <74423403+CristianGoFI@users.noreply.github.com>
2021-03-16 21:18:46 -0500

    cambios

--- RNP_Encoder1.1.py
+++ RNP_Encoder1.1.py
@@ -2,14 +2,14 @@
 # license removed for brevity
 """ importar librerias necesarias """
 import rospy
-from geometry_msgs.msg import Pose # importamos el tipo de dato pose
+from geometry_msgs.msg import Point # importamos el tipo de dato pose
 import RPi.GPIO as GPIO # libreria para comunicacion de puestos GPIO de la raspberry
 import time             # libreria para obtener el tiempo
 
 Enc=Pose() # Tipo de dato pose
 """ pines usados en la rapsberry"""
-RoAPin = 21    
-RoBPin = 20   
+RoAPin = 21
+RoBPin = 20
 """ variables """
 gain=360/(11*34*4)
 grados=0
@@ -22,17 +22,17 @@ def setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(RoAPin, GPIO.IN) # input mode
     GPIO.setup(RoBPin, GPIO.IN)
-    
+
 """ funcion que lee el encoder """
 def rotaryDeal():
-  
+
  global QEM
  global index
  global gain
  global grados
  global count
  global statep
-   
+
  A= GPIO.input(RoAPin)
  B= GPIO.input(RoBPin)
  if (A==1) and (B==1):
@@ -46,27 +46,27 @@ def rotaryDeal():
  index=4*state + statep
  if (count >= 1496) or (count<=-1496):
         count=0
- count=count + QEM[index]   
+ count=count + QEM[index]
  statep=state
 
  grados=count*gain
  return (grados)
-""" funcion que limpia los purtos utilizados """
+""" funcion que limpia los puertos utilizados """
 def destroy():
         GPIO.cleanup()
 """ funcion que publica los datos del encoder """
 def talker():
-    pub = rospy.Publisher('Encoder1', Pose, queue_size=10)
+    pub = rospy.Publisher('Encoder1', Point, queue_size=10)
     rospy.init_node('talker1', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(100) # 10hz
     while not rospy.is_shutdown():
-        
-        
+
+
         sensor=rotaryDeal()
-        Enc.position.x=sensor
+        Enc.x=sensor
         #Enc.position.y=3
         #Enc.position.z=12
-        rospy.loginfo(Enc)
+        #rospy.loginfo(Enc)
         pub.publish(Enc)
         rate.sleep()
 
