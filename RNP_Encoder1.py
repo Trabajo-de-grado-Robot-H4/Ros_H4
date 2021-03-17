263fbc7ef8478ce1bcbb53010d1f011e04e34ed1
CristianGoFI <74423403+CristianGoFI@users.noreply.github.com>
2021-03-16 21:12:38 -0500

    aunmento de envio de datos de 10hz a 100hz

--- RNP_Encoder1.py
+++ RNP_Encoder1.py
@@ -57,7 +57,7 @@ def destroy():
 def talker():
     pub = rospy.Publisher('Encoder1', Point, queue_size=10)
     rospy.init_node('talker1', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(100) # 10hz
     while not rospy.is_shutdown():
 
 
