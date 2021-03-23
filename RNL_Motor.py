2c462187f27196f8840b7c15be9799e305b1cc9f
CristianGoFI <74423403+CristianGoFI@users.noreply.github.com>
2021-03-23 15:54:31 -0500

    nnm,

--- RNL_Motor.py
+++ RNL_Motor.py
@@ -6,7 +6,7 @@ import rospy
 from geometry_msgs.msg import Point
 
 
-
+rospy.init_node('listener', anonymous=True)
 i2c = busio.I2C(board.SCL, board.SDA)
 pca = adafruit_pca9685.PCA9685(i2c)
     #asignamos la frecuencia del PWM
@@ -20,7 +20,7 @@ def callback(data):
     rospy.loginfo(rospy.get_caller_id() + 'I heard %f', variable_x)
 
 def listener():
-    rospy.init_node('listener', anonymous=True)
+
 
     # In ROS, nodes are uniquely named. If two nodes with the same
     # name are launched, the previous one is kicked off. The
