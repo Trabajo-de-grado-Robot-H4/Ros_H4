0f14565653b8815107a17374e4126b1b62ae2a92
Dfsuarez97 <74423258+Dfsuarez97@users.noreply.github.com>
2021-03-31 22:12:54 -0500

    Update RNP_Encoder1.2.py

--- RNP_Encoder1.2.py
+++ RNP_Encoder1.2.py
@@ -30,7 +30,7 @@ def setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(RoAPin, GPIO.IN) # input mode
     GPIO.setup(RoBPin, GPIO.IN)
-    GPIO.add_event_detect(RoAPin, GPIO.BOTH, callback=callbackEncoder)
+    GPIO.add_event_detect(RoAPin, GPIO.FALLING, callback=callbackEncoder)
 
 """ INTERRUPCIÓN ENCODERS """
 
