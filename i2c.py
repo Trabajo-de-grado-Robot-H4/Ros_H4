878d2f399a388f4d31b25725c76c8dd227849791
CristianGoFI <74423403+CristianGoFI@users.noreply.github.com>
2021-04-29 18:12:49 -0500

    cpdigo correcto

--- i2c.py
+++ i2c.py
@@ -9,21 +9,21 @@ bus = smbus.SMBus(1)
 # Direccion del i2c arduino esclavo
 address = 0x04
 
-
+'''
 def readLong():
    #number = bus.read_byte(address)
    number=""
    for i in range(4):
       number += chr(bus.read_byte(address))
    return struct.unpack('l', number)
-
-
 '''
+
+
 def readLong():
    block = bus.read_i2c_block_data(address, 1) #second arg is 'cmd'. It is andatory but not used in this case. It may be used by the higher level protocol
    n=struct.unpack('<l',bytes(block[:4]))[0]
    return n
- '''
+
 
 
 while True:
