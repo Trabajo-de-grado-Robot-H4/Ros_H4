e9a8d7e4dfd4dd8345db76cdabc679d2a1d99bf1
Dfsuarez97 <74423258+Dfsuarez97@users.noreply.github.com>
2021-05-01 23:57:22 -0500

    Update i2c.py

--- i2c.py
+++ i2c.py
@@ -16,7 +16,7 @@ def readLong():
    for i in range(10):
       number= number+chr(block[i])
       time.sleep(0.02)
-   print(read_word_data(address))
+   print(bus.read_word_data(address))
    return number
 
 
