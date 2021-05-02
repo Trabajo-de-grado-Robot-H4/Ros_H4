7cbce7d4c750059323187f3acdb65a74d4c8cac3
Dfsuarez97 <74423258+Dfsuarez97@users.noreply.github.com>
2021-05-02 18:03:01 -0500

    Update i2c.py

--- i2c.py
+++ i2c.py
@@ -12,10 +12,10 @@ address = 0x04
 
 def readLong():
    block = bus.read_i2c_block_data(address, 0,10)
-   number=""
-   for i in range(10):
-      number=number+chr(block[i])
-   return number#struct.unpack('f', block)[0]
+   #number=""
+   #for i in range(10):
+   #   number=number+chr(block[i])
+   return struct.unpack('f', block)[0:20]
 
 
 while True:
