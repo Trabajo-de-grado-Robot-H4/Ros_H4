573f87c09e46a72621d948a6782f62fae5a509ec
Dfsuarez97 <74423258+Dfsuarez97@users.noreply.github.com>
2021-05-02 15:13:20 -0500

    Update i2c.py

--- i2c.py
+++ i2c.py
@@ -14,16 +14,8 @@ def readLong():
    number=""
    for i in range(32):
       number= number+chr(block[i])
+      print(number)
    return number
-
-
-'''
-def readLong():
-   block = bus.read_i2c_block_data(address, 1) #second arg is 'cmd'. It is andatory but not used in this case. It may be used by the higher level protocol
-   n=struct.unpack('<l',bytes(block[:4]))[0]
-   return n
-'''
-
 
 while True:
 
