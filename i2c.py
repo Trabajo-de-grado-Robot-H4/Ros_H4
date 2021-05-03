eacd965bdcbeb511eb9a11b3684394e494a56691
CristianGoFI <74423403+CristianGoFI@users.noreply.github.com>
2021-05-03 16:33:12 -0500

    error areglado

--- i2c.py
+++ i2c.py
@@ -15,7 +15,7 @@ def readLong():
    #number=""
    #for i in range(10):
      # number=number+chr(block[i])
-   return struct.unpack('f', b"".join(map(chr, bytes[0;10])))[0]
+   return struct.unpack('f', b"".join(map(chr, bytes[0:10])))[0]
 
 
 while True:
