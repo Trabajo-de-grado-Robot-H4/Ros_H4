3924742d3e322e6aafb594ff93bffa8b68a053b0
CristianGoFI <74423403+CristianGoFI@users.noreply.github.com>
2021-05-03 16:27:04 -0500

    block [0,0]

--- i2c.py
+++ i2c.py
@@ -15,7 +15,7 @@ def readLong():
    #number=""
    #for i in range(10):
      # number=number+chr(block[i])
-   return struct.unpack('f', chr(block[1]))[0]
+   return struct.unpack('f', chr(block[0,0]))[0]
 
 
 while True:
