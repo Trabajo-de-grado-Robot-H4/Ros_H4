ac9d068bd29ba9df03caa84164ca87292a44fec6
Dfsuarez97 <74423258+Dfsuarez97@users.noreply.github.com>
2021-05-03 17:04:33 -0500

    Create i2c.py

--- i2c.py
+++ i2c.py
@@ -12,10 +12,12 @@ address = 0x04
 
 def readLong():
    block = bus.read_i2c_block_data(address, 0,10)
+   print(block)
+   
    number=""
    for i in range(10):
      number=number+chr(block[i])
-   return block
+   return number
 
 
 while True:
